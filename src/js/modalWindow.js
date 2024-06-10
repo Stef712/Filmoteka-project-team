b5b43c43a423f6f22d2abfcb17e06254da83cc09
danibatir <danibatir@gmail.com>
2024-06-10 14:16:20 +0300

    js la pagination totopbtn si modal window

---
+++ src/js/modalWindow.js
@@ -0,0 +1,31 @@
+var modal = document.getElementById('myModal');
+
+var modalTitle = document.getElementById('modalTitle');
+var modalDescription = document.getElementById('modalDescription');
+var modalImage = document.getElementById('modalImage');
+
+var span = document.getElementsByClassName('close')[0];
+
+function openModal(event) {
+  var img = event.target;
+  modalTitle.textContent = img.getAttribute('data-title');
+  modalDescription.textContent = img.getAttribute('data-description');
+  modalImage.src = img.src;
+  modalImage.alt = img.alt;
+  modal.style.display = 'block';
+}
+
+var images = document.getElementsByClassName('modal-trigger');
+for (var i = 0; i < images.length; i++) {
+  images[i].addEventListener('click', openModal);
+}
+
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
